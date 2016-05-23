39c79fcc75360d65dadb07f5a13c506605175ceb
Vitaliy Lyubota <v.lyubota1989@gmail.com>
2016-05-23 15:52:04 +0300

    Initial commit

---
+++ src/main/java/org/demo/Main.java
@@ -0,0 +1,12 @@
+package org.demo;
+
+/**
+ * Created by Виталик on 23.05.2016.
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+
+}
